2d31dee9a9d8da443b683b77b5b0455ec5cb5c60
davidcastro96 <davidcastro0796@outlook.es>
2022-12-29 22:37:14 -0500

    creación del proyecto en angular | fundamentos - Platzi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'my-store-angular';
+  name = 'david castro';
+  edad = 54;
+  img = 'https://fondosmil.com/fondo/55687.jpg';
+  btnDisabled: boolean = true;
+
+  btnSendEnable() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
 }
